bd65f14fe0851b3cda3f820d5ddecd9dcbc1c3de
Oketaiw <okeracheal911@gmail.com>
2023-01-21 15:01:40 +0100

    first commit

---
+++ src/Login.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>
+      Welcome 
+      sign in to your account
+      Hello
+      Good Day
+      Login Your account
+      Patient ID
+  Password
+  <input/> Remember Forget password?
+  <input/> SUBMIT
+  Create Account
+
+  </div>
+    
+  )
+}
---
+++ src/Register.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Register() {
+  return (
+    <div>
+     <input/>Dashboard
+     <input/>Alubumin
+     <input/>Alkaline phosphatse
+     <input/>Alanine transminase
+     <input/>Aspartate aminotransferase
+     <input/>Bilirubin
+     <input/>Cholinesterase
+     <input/>Cholestrol
+     <input/>Creatinine
+     <input/>Gamma-glutamyl transferase
+     <input/>Prothrombin 
+     Welcome 23456789
+     DIAGONSIS CATEGORY
+     Age <input/>
+     sex <input/>
+     <input/>HEPATITIS
+     <input/>FIBROSIS
+     <input/>CIRRHOSIS
+    </div>
+  )
+}

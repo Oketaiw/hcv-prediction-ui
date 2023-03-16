e279b24eea04e7b5a110bfb53667294cf40ccacc
Oketaiw <okeracheal911@gmail.com>
2023-03-16 21:16:41 +0100

    newchanges

--- src/Hcvtest.js
+++ src/Hcvtest.js
@@ -22,7 +22,7 @@ export default function Hcvtest() {
 
 	async function predict(predictionInput) {
 		console.log(predictionInput)
-		const response = await fetch("http://localhost:4000/predict", {
+		const response = await fetch("https://pr-fabd.onrender.com/predict", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
